806f678cdb3f61dff392c2cbfaca6434078895c4
Sumit <sk9373279@gmail.com>
2025-10-06 22:23:26 +0530

    minor profile change

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native'
 const Profile = () => {
   return (
     <View>
-      <Text>Profile hello i am sumit</Text>
+      <Text>Profile </Text>
     </View>
   )
 }
